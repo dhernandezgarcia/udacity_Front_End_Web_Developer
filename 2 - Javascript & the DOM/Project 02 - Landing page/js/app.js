eeba6d6113aba992f4387b72a943371e41720331
Daniel HG <danihergar@gmail.com>
2020-09-04 15:12:30 +0100

    feat: add initial navigation menu

--- 2 - Javascript & the DOM/Project 02 - Landing page/js/app.js
+++ 2 - Javascript & the DOM/Project 02 - Landing page/js/app.js
@@ -34,7 +34,17 @@
 */
 
 // build the nav
+const sections = document.getElementsByTagName('section');
+function createListElement (href, text) {
+    const newList = document.createElement('li');
+    newList.innerHTML = "'<a href='#" + href + "'>" + text + "</a>'";
+    return newList;
+}
 
+const ulNav = document.getElementById('navbar__list');
+for (let i=0; i < sections.length; i++) {
+    ulNav.appendChild(createListElement(sections[i].id, sections[i].dataset.nav));
+};
 
 // Add class 'active' to section when near top of viewport
 
