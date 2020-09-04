c7c7cc25e880a2dc8718ddc4cb4e5e155fb2a0ff
Daniel HG <danihergar@gmail.com>
2020-09-04 19:40:06 +0100

    feat: add smooth scrolling and styling in active sections

--- 2 - Javascript & the DOM/Project 02 - Landing page/js/app.js
+++ 2 - Javascript & the DOM/Project 02 - Landing page/js/app.js
@@ -32,6 +32,19 @@ function createListElement (href, text) {
     return newList;
 }
 
+// Helper to check if an element is in viewport
+// https://gomakethings.com/how-to-test-if-an-element-is-in-the-viewport-with-vanilla-javascript/
+// https://gist.github.com/davidtheclark/5515733
+function isInViewport (elem) {
+    var rect = elem.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document. documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document. documentElement.clientWidth)
+  );
+};
+
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -51,7 +64,24 @@ for (let i=0; i < sections.length; i++) {
 };
 
 // Add class 'active' to section when near top of viewport
+window.addEventListener('scroll', function () {
+    const sections = document.getElementsByTagName('section');
+    for (section of sections) {
+        const activeNavLi = document.getElementById(`section${section.dataset.nav.slice(8, 9)}`);
+        console.log(section.dataset.nav.slice(8, 9));
 
+        if (isInViewport(section)) {
+            section.classList.add('active_section');
+            activeNavLi.className = 'active_section';
+            console.log(`${section.dataset.nav} is in viewport!`);
+
+        } else {
+            section.classList.remove('active_section');
+            activeNavLi.className = '';
+            console.log(`${section.dataset.nav} is NOT in viewport!`);
+        }
+    }
+});
 
 // Scroll to anchor ID using scrollTO event
 
