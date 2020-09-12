d06e2a3099b64c5c3825daa5f31e2614a7921477
Daniel HG <danihergar@gmail.com>
2020-09-12 08:46:53 +0100

    feat: add API endpoint and weather API's integration
    For Project 03: Add API endpoint, integrate OpenWeatherMap API and fetch data from the app endpoint.

--- 3- Web APIs and Asynchronous/Project 03 - Weather Journal App/website/app.js
+++ 3- Web APIs and Asynchronous/Project 03 - Weather Journal App/website/app.js
@@ -1,5 +1,28 @@
 /* Global Variables */
 
+// Define base URL and API key for the Weather App
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip='
+const apiKey = '&APPID=2811e8a313297e9b8318cdfb32fbd223'
+
 // Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let d = new Date()
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear()
+
+// GET request to the Weather Info API
+document.getElementById('generate').addEventListener('click', performAction)
+
+function performAction(e) {
+  const zipCode = document.getElementById('zip').value
+  getWeatherInfo(baseURL, zipCode, apiKey)
+}
+
+const getWeatherInfo = async (baseURL, zipCode, key) => {
+  const res = await fetch(baseURL + zipCode + key)
+  try {
+    const data = await res.json()
+    console.log(data)
+    return data
+  } catch (error) {
+    console.log('Error occurred! ', error)
+  }
+}
